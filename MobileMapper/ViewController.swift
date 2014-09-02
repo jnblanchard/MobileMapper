3c55c7c9dd0cb3d962ed6725850097aad704fd5c
John Blanchard <jnblanchard@mac.com>
2014-09-02 09:26:18 -0500

    first commit

---
+++ MobileMapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileMapper
+//
+//  Created by John Blanchard on 8/5/14.
+//  Copyright (c) 2014 John Blanchard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
